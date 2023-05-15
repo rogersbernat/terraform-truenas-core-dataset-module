ca43a8e09b8cb152342ac1e5121693926dd7b6a6
roger.sanchez <roger.sanchez@edreamsodigeo.com>
2023-05-15 21:01:19 +0200

    RSB - add examples variables.tf

---
+++ example/variables.tf
@@ -0,0 +1,15 @@
+variable "nested_datasets" {
+  description = "List of dataset names"
+  type        = list(string)
+  default     = []
+}
+
+variable "base_url" {
+  description = "Base URL of the TrueNAS server API"
+  type        = string
+}
+
+variable "api_key" {
+  description = "API key for authenticating to the TrueNAS server"
+  type        = string
+
